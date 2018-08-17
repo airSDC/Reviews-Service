8f9720b1e0d6f88c5a67bf90a9c66806d5a07bce
Allison Chen <allisonc07@gmail.com>
2018-08-17 12:06:08 -0700

    Connect database to server

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,11 +2,11 @@ const models = require('../models');
 
 module.exports = {
   get: (req, res) => {
-    models.Review.find({ room_id: Number(req.params.roomId) }, (err, reviews) => {
+    models.getReviewsByRoomId([req.params.roomId], (err, reviews) => {
       if (err) {
         res.status(500).send(err);
       } else {
-        res.status(200).send(reviews);
+        res.status(200).send(reviews.rows);
       }
     });
   },
